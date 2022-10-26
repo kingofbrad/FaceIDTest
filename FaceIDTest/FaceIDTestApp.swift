5f034e1d7bcf8516fce0414440dfe9915117e2c0
Bradlee King <Kingofbrad@gmail.com>
2022-10-26 18:30:28 +0100

    Initial Commit

---
+++ FaceIDTest/FaceIDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDTestApp.swift
+//  FaceIDTest
+//
+//  Created by Bradlee King on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
